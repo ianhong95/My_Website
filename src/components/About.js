d8bf816cf802be4205a234a8e1ad7dfa860ef606
Ian Hong <ianqyhong@gmail.com>
2023-01-22 10:33:56 -0500

    Testing new build

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,8 @@ const About = () => {
     <div className="about-section">
       <h2 className="text-white mb-4">About Me</h2>
       <p className="text-white-50-left">
-        Hi, I'm Ian. I studied Mechanical Engineering at McMaster University, and have always had a strong interest in automation.
-        I've recently discovered my love for coding through experimental projects in my free time. I find joy in building things, whether it be silly robots or scripts/programs for automation.
+      Hi, I'm Ian! I studied Mechanical Engineering at McMaster University, and have always had a strong interest in automation and robotics.
+        In my free time, I'm either building something or playing piano. I'm always trying to learn something new, which is why I've been learning to code through online resources and personal projects. With technology advancing so quickly, there is always something new and exciting to play around with.
       </p>
       <img className="about-img" src="assets/img/portrait.jpg" alt="..." id="test"/>
     </div>
